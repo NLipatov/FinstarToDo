19c0eba8e800afe938a792b8d9c3889435c41518
Nikita Lipatov <6stringsohei@gmail.com>
2023-03-01 19:50:50 +0300

    New 'Commentaries' property related migration

--- FinstarToDo/Migrations/20230301164747_ToDoCommentariesPropertyAdded.Designer.cs
+++ FinstarToDo/Migrations/20230301164747_ToDoCommentariesPropertyAdded.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using FinstarToDo.DB;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 
@@ -11,9 +12,11 @@ using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 namespace FinstarToDo.Migrations
 {
     [DbContext(typeof(ToDoContext))]
-    partial class ToDoContextModelSnapshot : ModelSnapshot
+    [Migration("20230301164747_ToDoCommentariesPropertyAdded")]
+    partial class ToDoCommentariesPropertyAdded
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ FinstarToDo/Migrations/20230301164747_ToDoCommentariesPropertyAdded.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace FinstarToDo.Migrations
+{
+    /// <inheritdoc />
+    public partial class ToDoCommentariesPropertyAdded : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
